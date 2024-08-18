585dafc5f14e6b28e63cfc7359b007317be83b3b
Ahmed Yasser <ahmdyassr@gmail.com>
2024-08-18 19:34:24 +0300

    Rename selectbar component

---
+++ app/_components/Selectbar.jsx
@@ -0,0 +1,18 @@
+import styles from '@/scss/components/Selectbar.module.scss'
+
+const Selectbar = ({ label, options, value, onChange }) => {
+	return (
+		<div className={styles.select}>
+			<span className={styles.select__label}>{label}</span>
+			<span className={styles.select__box}>
+				{options.map(({ label, value: optionValue }) => (
+					<span
+						className={`${styles.select__tab} ${optionValue === value && styles.isActive}`}
+						onClick={() => onChange(optionValue)}>{label}</span>
+				))}
+			</span>
+		</div>
+	)
+}
+
+export default Selectbar
