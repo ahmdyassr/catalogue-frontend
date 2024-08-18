a155db7c3aac7a97df00eecc71ca24dfde13a911
Ahmed Yasser <ahmdyassr@gmail.com>
2024-08-18 18:13:23 +0300

    Add textarea component

---
+++ app/_components/Textarea.jsx
@@ -0,0 +1,12 @@
+import styles from '@/scss/components/Textarea.module.scss'
+
+const Textarea = ({ label, value, onChange }) => {
+	return (
+		<div className={styles.textarea}>
+			<label className={styles.textarea__label}>{label}</label>
+			<textarea className={styles.textarea__box} onChange={onChange}>{value}</textarea>
+		</div>
+	)
+}
+
+export default Textarea
