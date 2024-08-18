bdff4a69d211149f71ac5bda834007b9db885eab
Ahmed Yasser <ahmdyassr@gmail.com>
2024-08-18 21:45:20 +0300

    Add dummy homepage

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
-const Home = async () => {
+const Page = async () => {
 	return (
-		<>
-			<p>Homepage</p>
-		</>
+		<div style={{
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: '100%',
+			padding: '16px'
+		}}>
+			<p style={{
+				color: 'var(--light-700)',
+				fontWeight: 'var(--semibold)',
+				fontSize: '32px'
+			}}>🛖</p>
+		</div>
 	)
 }
 
-export default Home
+export default Page
