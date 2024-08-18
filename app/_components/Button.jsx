b1d82ef2e4859462cecb28b8bc710b265287c1d6
Ahmed Yasser <ahmdyassr@gmail.com>
2024-08-18 18:46:08 +0300

    Add disability to buttons

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -2,13 +2,14 @@
 
 import styles from '@/scss/components/Button.module.scss'
 
-const Button = ({ text, type, onClick }) => {
+const Button = ({ text, type, isDisabled, onClick }) => {
 	return (
 		<button
 			className={`
 				${styles.button} 
 				${type === 'secondary' && styles.secondary}
 				${type === 'danger' && styles.danger}`}
+			disabled={isDisabled}
 			onClick={onClick}>
 			{text}
 		</button>
